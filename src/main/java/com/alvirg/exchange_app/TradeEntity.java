9eb45add5567e7fac7ce5c7206b7528c2b1485cb
albertegi <albertegi90@gmail.com>
2025-06-27 23:00:58 +0100

    create TradeEntity class

---
+++ src/main/java/com/alvirg/exchange_app/TradeEntity.java
@@ -0,0 +1,33 @@
+package com.alvirg.exchange_app;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+public class TradeEntity extends BaseEntity {
+
+    private int offerorQuantity;
+    private int offereeQuantity;
+
+    @Enumerated(EnumType.STRING)
+    private TradeStatus status;
+
+//    @ManyToOne
+//    @JoinColumn(name = "offeror_listing_id")
+//    private ListingEntity offerorListing;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "offeree_listing_id")
+//    private ListingEntity offereeListing;
+}
