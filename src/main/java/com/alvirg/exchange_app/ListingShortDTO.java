0e2574dd0bca42b32e9709cb9cffb6e80e4def05
albertegi <albertegi90@gmail.com>
2025-06-28 21:05:35 +0100

    update ListingShortDTO and TradeMapper

--- src/main/java/com/alvirg/exchange_app/ListingShortDTO.java
+++ src/main/java/com/alvirg/exchange_app/ListingShortDTO.java
@@ -1,8 +1,12 @@
 package com.alvirg.exchange_app;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ListingShortDTO {
     private Integer id;
     private String name;
--- src/main/java/com/alvirg/exchange_app/TradeMapper.java
+++ src/main/java/com/alvirg/exchange_app/TradeMapper.java
@@ -1,4 +1,36 @@
 package com.alvirg.exchange_app;
 
 public class TradeMapper {
+
+    public static TradeResponseDTO toTradeResponseDTO(
+            TradeEntity tradeEntity,
+            ListingEntity offerorListing,
+            ListingEntity offereeListing ){
+
+        if(tradeEntity == null) return null;
+
+
+        var tradeResponseDto = new TradeResponseDTO();
+        tradeResponseDto.setOfferorQuantity(tradeEntity.getOfferorQuantity());
+        tradeResponseDto.setOffereeQuantity(tradeEntity.getOffereeQuantity());
+        tradeResponseDto.setStatus(tradeEntity.getStatus());
+
+        tradeResponseDto.setOfferorListing(toListingShortDTO(offerorListing));
+        tradeResponseDto.setOffereeListing(toListingShortDTO(offereeListing));
+
+        return tradeResponseDto;
+
+    }
+
+    // convert ListingEntity → ListingShortDTO
+
+    public static ListingShortDTO toListingShortDTO(ListingEntity listingEntity){
+        if(listingEntity == null) return null;
+
+        return new ListingShortDTO(
+                listingEntity.getId(),
+                listingEntity.getName(),
+                listingEntity.getImageUrl());
+
+    }
 }
