155ed691082604a300eb611a393a3ac414241a49
albertegi <albertegi90@gmail.com>
2025-06-28 19:28:14 +0100

    update ListingMapper with mappings for CreateListingDTO to entity

--- src/main/java/com/alvirg/exchange_app/ListingMapper.java
+++ src/main/java/com/alvirg/exchange_app/ListingMapper.java
@@ -1,4 +1,20 @@
 package com.alvirg.exchange_app;
 
 public class ListingMapper {
+
+    public static ListingEntity fromCreateListingDTO(CreateListingDTO dto){
+        if(dto == null) return null;
+
+        var listingEntity = new ListingEntity();
+        listingEntity.setName(dto.getName());
+        listingEntity.setDescription(dto.getDescription());
+        listingEntity.setCondition(dto.getCondition());
+        listingEntity.setQuantity(dto.getQuantity());
+        listingEntity.setImageUrl(dto.getImageUrl());
+        listingEntity.setVisible(dto.isVisible());
+        listingEntity.setId(dto.getUserId());
+        listingEntity.setId(dto.getCategoryId());
+
+        return listingEntity;
+    }
 }
