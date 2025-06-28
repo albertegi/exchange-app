56a89690027ea6d77ac91f428239408b197d0d0b
albertegi <albertegi90@gmail.com>
2025-06-28 15:11:26 +0100

    create CategoryDTO

---
+++ src/main/java/com/alvirg/exchange_app/CategoryDTO.java
@@ -0,0 +1,11 @@
+package com.alvirg.exchange_app;
+
+import lombok.Data;
+
+@Data
+public class CategoryDTO {
+    private Integer id;
+    private String name;
+    private String description;
+}
+
