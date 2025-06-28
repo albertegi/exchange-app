59ae0435c9d0369ae8b8e8f6984533924d9ef11b
albertegi <albertegi90@gmail.com>
2025-06-28 15:04:37 +0100

    create UserDTO

---
+++ src/main/java/com/alvirg/exchange_app/UserDTO.java
@@ -0,0 +1,12 @@
+package com.alvirg.exchange_app;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
+
